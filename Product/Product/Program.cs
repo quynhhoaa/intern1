2a21b9bee671f8d4937a83669940c6e346b45d68
quynhhoaa <quynhhoa020305@gmail.com>
2023-01-09 21:36:34 +0700

    test1

--- Product/Product/Program.cs
+++ Product/Product/Program.cs
@@ -6,7 +6,7 @@ namespace Product
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test!");
         }
     }
 }
