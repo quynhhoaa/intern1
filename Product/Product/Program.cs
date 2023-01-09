c5a2b649e52925ebe3d367a9602f335bb96fd246
quynhhoaa <quynhhoa020305@gmail.com>
2023-01-09 23:08:48 +0700

    Mini_max_sum

--- Product/Product/Program.cs
+++ Product/Product/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Product
+class Result
 {
-    class Program
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
     {
-        static void Main(string[] args)
+        long sum = 0;
+        arr.Sort();
+        for (int i = 0; i < arr.Count; i++)
         {
-            Console.WriteLine("Hello World!");
+            sum += (long)arr[i];
         }
+        Console.Write("{0} {1}", (sum - arr[4]), (sum - arr[0]));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
     }
 }
