2bfa91f591378f10505a4ed3a2fcb88bb8749ecf
quynhhoaa <quynhhoa020305@gmail.com>
2023-01-09 23:22:11 +0700

    Compare_the_Triplets

--- Product/Product/Program.cs
+++ Product/Product/Program.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Product
+class Result
 {
-    class Program
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
     {
-        static void Main(string[] args)
+        int cnt = a.Count;
+        int kqa = 0, kqb = 0;
+        for(int i = 0; i< cnt; i++)
         {
-            Console.WriteLine("Hello World!");
+            if(a[i] > b[i])
+                kqa++;
+            else if(a[i] < b[i])
+                    kqb++;
         }
+        List<int> c = new List<int>();
+        c.Add(kqa);
+        c.Add(kqb);
+        return c;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
